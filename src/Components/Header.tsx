5adc6de9dc58c93bc6c0576de2d00efa1c77dd61
Junnygram <you@example.com>
2023-08-04 23:25:29 +0100

    updating...

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link';
 import { AiOutlineMenu, AiOutlineClose, AiFillHome } from 'react-icons/ai';
 import { FcAbout } from 'react-icons/fc';
 import { BsFillPersonFill } from 'react-icons/bs';
+import { BiLogIn } from 'react-icons/bi';
+
 import { usePathname } from 'next/navigation';
 
 export const MenuItems = ({ name, url }: { name: string; url: string }) => {
@@ -16,7 +18,7 @@ export const MenuItems = ({ name, url }: { name: string; url: string }) => {
       <div
         className={` hover:text-[#d8a642] ${
           pathname == url ? 'text-[#d8a642]' : 'text-slate-950'
-        }`}
+        } `}
       >
         {name}
       </div>
@@ -31,7 +33,7 @@ export const Header = () => {
     setNav(!nav);
   };
   return (
-    <div className="bg-white h-16 ">
+    <div className="h-16 ">
       <div className="flex w-4/5 mx-auto justify-between items-center h-full">
         <div className="w-8">
           <Image src="/assets/sug.jpeg" width={200} height={100} alt="Logo" />
@@ -56,7 +58,7 @@ export const Header = () => {
             <div
               className={
                 nav
-                  ? 'fixed right-2 top-0 w-[60%] h-full py-4 ease-in-out bg-white transition  delay-200 duration-500'
+                  ? 'fixed z-10 right-2 bg-slate-400 top-0 w-[60%] h-screen py-4 ease-in-out  transition  delay-200 duration-500'
                   : 'ease-in-out duration-500 fixed right-[-100%]'
               }
             >
@@ -94,6 +96,14 @@ export const Header = () => {
                   <BsFillPersonFill size={20} />
                   <span className="pl-4">Candidate</span>
                 </Link>
+                <Link
+                  onClick={handleNav}
+                  href="/login"
+                  className="w-[75%] flex mx-auto justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+                >
+                  <BiLogIn size={20} />
+                  <span className="pl-4">Login</span>
+                </Link>
               </div>
             </div>
           ) : (
@@ -101,10 +111,11 @@ export const Header = () => {
           )}
         </div>
 
-        <div className=" items-center hidden md:flex w-2/5  justify-between ">
+        <div className=" items-center hidden md:flex w-2/5  justify-between  ">
           <MenuItems name="Home" url="/" />
           <MenuItems name="About" url="/about" />
           <MenuItems name="Candidates" url="/candidates" />
+          <MenuItems name="Login" url="/login" />
         </div>
       </div>
     </div>
